0cd769ec1f8a9d34798f2cb257eb75915e7c71a2
SebasN12 <snagles2015@gmail.com>
2026-01-25 22:18:35 +0100

    Add option for separate test dataset in data preparation

--- src/deepL/data_preparation.py
+++ src/deepL/data_preparation.py
@@ -6,6 +6,7 @@ RANDOM_SEED = 42
 # Change these variables if using other dataset
 isOtherDataset = False
 datasetPath = 'dataset/all_news.csv'
+datasetPath_test = 'dataset/all_news.csv'   # only if other dataset is used for testing
 
 
 def run_data_preparation():
@@ -40,5 +41,50 @@ def run_data_preparation():
     test_df.to_csv("dataset/test_DL.csv", index=False)
     print("Data preparation completed. Train and test CSV files created.")
 
+def run_data_preparation_more_df():
+    
+    df = pd.read_csv(datasetPath)
+    df_test = pd.read_csv(datasetPath_test)
+
+    df["text"] = (
+        df["title"].fillna("") + " " +
+        df["text"].fillna("")
+    )
+
+    df_test["text"] = (
+        df_test["title"].fillna("") + " " +
+        df_test["text"].fillna("")
+    )
+
+    df_test = df_test.sample(frac=1, random_state=RANDOM_SEED).reset_index(drop=True)
+    x_test = df_test["text"]
+    y_test = df_test["is_fake"]
+
+    x = df["text"]
+    y = df["is_fake"]
+
+    train_df = pd.DataFrame({
+        "text": x,
+        "label": y
+    })
+
+    test_df = pd.DataFrame({
+        "text": x_test,
+        "label": y_test
+    })
+
+    train_df.to_csv("dataset/train_DL.csv", index=False)
+    test_df.to_csv("dataset/test_DL.csv", index=False)
+    print("Data preparation completed. Train and test CSV files created.")
+
+
 if __name__ == "__main__":
-    run_data_preparation()
+    # change this if using other dataset for test. 
+    use_other_dataset_test = False
+
+    if use_other_dataset_test:
+        print("Preparing train/test with two separate datasets...")
+        run_data_preparation_more_df()
+    else:
+        print("Preparing train/test split from the same dataset...")
+        run_data_preparation()
